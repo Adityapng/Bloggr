414651c5ac2456744b08d2026d48fc021f417a45
Aditya <102513627+Adityapng@users.noreply.github.com>
2025-09-15 17:23:10 +0530

    testing db connection

--- backend/config/connection.ts
+++ backend/config/connection.ts
@@ -1,3 +1,17 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async (): Promise<void> => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI as string);
+//     console.log("MongoDB Connected");
+//   } catch (error) {
+//     console.error((error as Error).message);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
+
 import mongoose from "mongoose";
 
 let cachedConnection: typeof mongoose | null = null;
