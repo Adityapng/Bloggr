947c04a87006897aa26ae7a6540b4f1a12c3476d
Aditya <102513627+Adityapng@users.noreply.github.com>
2025-09-17 21:59:29 +0530

    made changes to restrict create post page from beig rendered to anon users

--- frontend/src/app/write/page.tsx
+++ frontend/src/app/write/page.tsx
@@ -29,6 +29,30 @@ export default function WritePage() {
     Record<string, string[]>
   >({});
   const [areTagsLoading, setAreTagsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    async function checkauth() {
+      try {
+        const response = await apiFetcher("/");
+        const data = await response.json();
+        if (data.isLoggedIn === true) {
+          console.log(data.isLoggedIn);
+
+          setIsAuthenticated(true);
+        } else {
+          console.log(data.isLoggedIn);
+          setIsAuthenticated(false);
+        }
+      } catch (error) {
+        console.log(error, "this is error msg");
+
+        setIsAuthenticated(false);
+      }
+    }
+
+    checkauth();
+  }, []);
 
   const MAX_WORDS = 10000;
 
@@ -155,114 +179,119 @@ export default function WritePage() {
     handleCreatePost("published");
   };
 
-  return (
-    <div className="min-h-screen w-full bg-gray-100 dark:bg-black flex flex-col items-center p-4">
-      {isLoading ? (
-        <div className=" w-full h-full flex items-center justify-center">
-          <Spinner className="mr-2 h-12 w-12" />
-        </div>
-      ) : (
-        <div className="w-full lg:w-1/2 max-w-4xl space-y-6">
-          {error && (
-            <div className="mt-4 text-red-600 bg-red-100 border border-red-400 rounded p-2">
-              {error}
-            </div>
-          )}
-          <div className=" flex justify-between items-center">
-            <h1 className="md:text-3xl text-2xl font-bold">Create Post</h1>
-            <div className=" flex gap-0.5">
-              <Button
-                onClick={handleSaveDraftClick}
-                disabled={isLoading}
-                className=" rounded-l-3xl rounded-r-xs  bg-green-700 hover:bg-green-800 text-white"
-              >
-                {isLoading ? "Saving..." : "Save Draft"}
-              </Button>
-              <Button
-                onClick={handlePublishClick}
-                disabled={isLoading}
-                className=" rounded-r-3xl rounded-l-xs bg-green-700 hover:bg-green-800 text-white"
-              >
-                {isLoading ? "Publishing..." : "Publish Post"}
-              </Button>
-            </div>
+  if (isAuthenticated === false) {
+    router.replace("/auth/signup");
+  } else if (isAuthenticated === true) {
+    return (
+      <div className="min-h-screen w-full bg-gray-100 dark:bg-black flex flex-col items-center p-4">
+        {isLoading ? (
+          <div className=" w-full h-full flex items-center justify-center">
+            <Spinner className="mr-2 h-12 w-12" />
           </div>
-          <Input
-            placeholder="Enter your title here..."
-            className="text-base"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            onFocus={(e) => e.stopPropagation()}
-            disabled={isLoading}
-          />
-          <div className="relative  w-full border rounded-lg overflow-hidden">
-            <SimpleEditor
-              onUpdate={setContent}
-              onImageUpload={handleImageUploadCallback}
-              onWordCountChange={setWordCount}
-            />
-          </div>
-
-          <div>
-            {areTagsLoading ? (
-              <p>Loading Tags...</p>
-            ) : (
-              <div>
-                <div className=" space-y-2">
-                  <Label htmlFor="tag" className=" text-lg pl-1">
-                    Tags
-                  </Label>
-                  <div className="relative">
-                    <Tags className="absolute left-3 top-2.5 h-4 w-4 text-muted-foreground" />
-                    {/* search tag */}
-                    <Input
-                      id="tag"
-                      type="text"
-                      placeholder="Add a tag..."
-                      onChange={(e) => handleTagSearch(e.target.value)}
-                      onFocus={(e) => e.stopPropagation()}
-                      className={`pl-10`}
-                      disabled={isLoading}
-                    />
-                  </div>
-                  {/* tag search result section */}
-                  <div className=" flex flex-wrap gap-2">
-                    {tagSearchResult &&
-                      tagSearchResult.map((tag) => (
-                        <button
-                          className="bg-yellow-500 hover:bg-yellow-600 rounded-full p-2 capitalize"
-                          key={`${tag}-search-results`}
-                          onClick={() => handleAddTag(tag)}
-                        >
-                          {tag}
-                        </button>
-                      ))}
-                  </div>
-                </div>
-                {/* selected tag display section */}
-                <div className=" flex gap-2 w-full flex-wrap p-4">
-                  {selectedTags.map((tag) => (
-                    <div
-                      className="py-2 px-2  bg-green-700 hover:bg-green-800 rounded-4xl flex items-center gap-2"
-                      key={`${tag}-selected-tag`}
-                    >
-                      <p className=" capitalize"> {tag}</p>
-                      <button
-                        className="bg-green-700 hover:bg-green-800 rounded-full "
-                        onClick={() => {
-                          handleRemoveTag(tag);
-                        }}
-                      >
-                        <X />
-                      </button>
-                    </div>
-                  ))}
-                </div>
+        ) : (
+          <div className="w-full lg:w-1/2 max-w-4xl space-y-6">
+            {error && (
+              <div className="mt-4 text-red-600 bg-red-100 border border-red-400 rounded p-2">
+                {error}
               </div>
             )}
+            <div className=" flex justify-between items-center">
+              <h1 className="md:text-3xl text-2xl font-bold">Create Post</h1>
+              <div className=" flex gap-0.5">
+                <Button
+                  onClick={handleSaveDraftClick}
+                  disabled={isLoading}
+                  className=" rounded-l-3xl rounded-r-xs  bg-green-700 hover:bg-green-800 text-white"
+                >
+                  {isLoading ? "Saving..." : "Save Draft"}
+                </Button>
+                <Button
+                  onClick={handlePublishClick}
+                  disabled={isLoading}
+                  className=" rounded-r-3xl rounded-l-xs bg-green-700 hover:bg-green-800 text-white"
+                >
+                  {isLoading ? "Publishing..." : "Publish Post"}
+                </Button>
+              </div>
+            </div>
+            <Input
+              placeholder="Enter your title here..."
+              className="text-base"
+              type="text"
+              onChange={(e) => setTitle(e.target.value)}
+              onFocus={(e) => e.stopPropagation()}
+              disabled={isLoading}
+            />
+            <div className="relative  w-full border rounded-lg overflow-hidden">
+              <SimpleEditor
+                onUpdate={setContent}
+                onImageUpload={handleImageUploadCallback}
+                onWordCountChange={setWordCount}
+                initialContent=""
+              />
+            </div>
+
+            <div>
+              {areTagsLoading ? (
+                <p>Loading Tags...</p>
+              ) : (
+                <div>
+                  <div className=" space-y-2">
+                    <Label htmlFor="tag" className=" text-lg pl-1">
+                      Tags
+                    </Label>
+                    <div className="relative">
+                      <Tags className="absolute left-3 top-2.5 h-4 w-4 text-muted-foreground" />
+                      {/* search tag */}
+                      <Input
+                        id="tag"
+                        type="text"
+                        placeholder="Add a tag..."
+                        onChange={(e) => handleTagSearch(e.target.value)}
+                        onFocus={(e) => e.stopPropagation()}
+                        className={`pl-10`}
+                        disabled={isLoading}
+                      />
+                    </div>
+                    {/* tag search result section */}
+                    <div className=" flex flex-wrap gap-2">
+                      {tagSearchResult &&
+                        tagSearchResult.map((tag) => (
+                          <button
+                            className="bg-yellow-500 hover:bg-yellow-600 rounded-full p-2 capitalize"
+                            key={`${tag}-search-results`}
+                            onClick={() => handleAddTag(tag)}
+                          >
+                            {tag}
+                          </button>
+                        ))}
+                    </div>
+                  </div>
+                  {/* selected tag display section */}
+                  <div className=" flex gap-2 w-full flex-wrap p-4">
+                    {selectedTags.map((tag) => (
+                      <div
+                        className="py-2 px-2  bg-green-700 hover:bg-green-800 rounded-4xl flex items-center gap-2"
+                        key={`${tag}-selected-tag`}
+                      >
+                        <p className=" capitalize"> {tag}</p>
+                        <button
+                          className="bg-green-700 hover:bg-green-800 rounded-full "
+                          onClick={() => {
+                            handleRemoveTag(tag);
+                          }}
+                        >
+                          <X />
+                        </button>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-      )}
-    </div>
-  );
+        )}
+      </div>
+    );
+  }
 }
